062d1351c5882f94b5ef8d2ae1798a7343d677a7
Rahul Jadhav <nyrahul@gmail.com>
2021-07-02 14:48:46 +0000

    wildcard based pattern matching in ebpf code

--- examples/c/patmatch.bpf.c
+++ examples/c/patmatch.bpf.c
@@ -6,6 +6,8 @@
 #include <bpf/bpf_core_read.h>
 
 char LICENSE[] SEC("license") = "Dual BSD/GPL";
+//const char wc[] = "*p";
+const char wc[] = "/home/*/*/????/*p";
 
 #if 0
 SEC("kprobe/do_execve")
@@ -15,10 +17,13 @@ int BPF_KPROBE(do_execve, const char *name,
 {
 	pid_t pid;
 	char comm[64];
+	char filename[256];
 
 	pid = bpf_get_current_pid_tgid() >> 32;
 	bpf_get_current_comm(&comm, sizeof(comm));
-	bpf_printk("KPROBE ENTRY pid = %d, filename = %s\n", pid, comm);
+	bpf_probe_read_kernel_str(filename, sizeof(filename), (void *)name);
+	//bpf_probe_read_user_str(filename, sizeof(filename), (const char*)ctx->args[0]);
+	bpf_printk("=------KPROBE ENTRY pid=%d, comm=%s\n", pid, filename);
 	return 0;
 }
 #endif
@@ -28,6 +33,7 @@ int BPF_KPROBE(do_execve, const char *name,
 #define CHK_TWO  \
     if (!wild[w] || !str[i])     \
         goto CHK_TWO_OUT;                                    \
+	two++;\
                                                              \
     if (wild[w] == '*') {                                    \
         if (!wild[++w]) return 1;                            \
@@ -47,13 +53,22 @@ int BPF_KPROBE(do_execve, const char *name,
  */
 static int wildcardMatch(const char* str, const char* wild)
 {
-    int i = 0, w = 0, mp = 0, cp = 0;
+    int i = 0, w = 0, mp = 0, cp = 0, one = 0, two = 0, three = 0;
 
 #define CHK_ONE \
     if (!str[i] || wild[i] == '*') goto CHK_ONE_OUT;\
     if ((wild[i] != str[i]) && (wild[i] != '?')) return 0;\
+	one++;\
     i++;
 
+    CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
+    CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
+    CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
+    CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
+    CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
+    CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
+    CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
+    CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
     CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
     CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
     CHK_ONE    CHK_ONE     CHK_ONE    CHK_ONE
@@ -73,12 +88,29 @@ static int wildcardMatch(const char* str, const char* wild)
     CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
     CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
     CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
+    CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
+    CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
+    CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
+    CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
+    CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
+    CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
+    CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
+    CHK_TWO    CHK_TWO    CHK_TWO    CHK_TWO
     CHK_TWO_OUT:
 
 #define CHK_THREE    \
     if (wild[w] != '*') goto CHK_THREE_OUT;\
-        w++;
+	three++;\
+	w++;
 
+    CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
+    CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
+    CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
+    CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
+    CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
+    CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
+    CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
+    CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
     CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
     CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
     CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
@@ -89,10 +121,14 @@ static int wildcardMatch(const char* str, const char* wild)
     CHK_THREE    CHK_THREE    CHK_THREE    CHK_THREE
     CHK_THREE_OUT:
 
+	if (!wild[w]) {
+		bpf_printk("one=%d, two=%d, three=%d\n", one, two, three);
+	}
+
     return !wild[w];
 }
 
-const char wc[] = "sle*ep";
+#if 0
 SEC("kretprobe/do_execve")
 int BPF_KRETPROBE(do_execve_exit)
 {
@@ -103,6 +139,19 @@ int BPF_KRETPROBE(do_execve_exit)
 	bpf_get_current_comm(&comm, sizeof(comm));
 	if (wildcardMatch(comm, wc)) {
 		bpf_printk("KRETPROBE ENTRY pid = %d, filename = %s\n", pid, comm);
+	}
+	return 0;
+}
+#endif
+
+SEC("tracepoint/syscalls/sys_enter_execve")
+int tracepoint__syscalls__sys_enter_execve(struct trace_event_raw_sys_enter* ctx)
+{
+	char filename[256];
+
+	bpf_probe_read_user_str(filename, sizeof(filename), (const char*)ctx->args[0]);
+	if (wildcardMatch(filename, wc)) {
+		bpf_printk("TRACEPOINT ENTRY filename=%s\n", filename);
 	}
 	return 0;
 }
